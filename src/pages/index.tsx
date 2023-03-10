1c0ca80a537d4624c00aa5b52a3cba8e1a2375de
Eberson Silva <binho.ebs@gmail.com>
2023-03-09 21:50:48 -0300

    Comunicação com Stripe concluida

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,18 @@ import camiseta4 from '../assets/camisetas/4.png'
 
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from '../lib/stripe'
+import Stripe from 'stripe'
 
-export default function Home(props) {
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+  }[]
+}
+
+export default function Home({ products }) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -21,48 +31,41 @@ export default function Home(props) {
   })
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={camiseta1} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={camiseta2} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={camiseta3} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-      <Product className="keen-slider__slide">
-        <Image src={camiseta4} width={520} height={480} alt="" />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+      {products.map(product => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
     </HomeContainer>
   )
 }
 
 //Configuração para resnderizar os dados somente depois que todas as informações foram carregadas.
 export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await stripe.products.list()
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
 
-  console.log(response.data)
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
 
-  
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100
+    }
+  })
+
   return {
     props: {
-      list: [1, 2, 3]
+      products
     }
   }
 }
