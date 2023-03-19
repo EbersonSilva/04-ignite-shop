118c3f7f32a12275a1943c00349e9d802dc1cd03
Eberson Silva <binho.ebs@gmail.com>
2023-03-19 15:01:07 -0300

    Criação e estilização da pagina de sucess

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link'
-import { ImageContainer } from '../styles/pages/product'
-import { SuccessContainer } from '../styles/pages/success'
+import { ImageContainer, SuccessContainer } from '../styles/pages/success'
 
 export default function Success() {
   return (
     <SuccessContainer>
-      <h1>Compra efetuada com sucesso!</h1>
+      <h1>Compra efetuada!</h1>
       <ImageContainer></ImageContainer>
       <p>
         Uhuul <strong>Eberson Silva</strong>, <strong></strong> sua Camisa ja
@@ -13,5 +12,6 @@ export default function Success() {
       </p>
       <Link href={'/'}>Voltar ao catalogo</Link>
     </SuccessContainer>
+    
   )
 }
